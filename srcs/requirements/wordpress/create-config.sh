bc2a7a0acf8d3a945fd17cbc71ac9484fda80659
sansho88 <sansho88@hotmail.fr>
2023-05-16 16:46:46 +0200

    env variables fonctionnent

--- srcs/requirements/wordpress/create-config.sh
+++ srcs/requirements/wordpress/create-config.sh
@@ -1,18 +1,18 @@
 echo "NjIKMOUK"
 if [ ! -f "/var/www/html/wp-config.php" ];
 then
-    while ! mysql -h mariadb --user=tgriffit --password=42School -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='dbName'";do
+    while ! mysql -h mariadb --user=$USER --password=$PASS_USER -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$DB_NAME'";do
             echo "Database is creating..."
             sleep 8;
     done
     wp core download --allow-root
-    wp config create --allow-root --dbname=dbName --dbuser=tgriffit --dbpass=42School --dbhost=maria --extra-php --force --skip-check
+    wp config create --allow-root --dbname=${DB_NAME} --dbuser=${USER} --dbpass=${PASS_USER} --dbhost=maria --extra-php --force --skip-check
     echo "bonjour"
-    wp core install --url=tgriffit.42.fr --title="My Awsome Wordpress Website" --admin_user=tgriffit --admin_password=42School --skip-email
-    wp user create --allow-root tgriffit tgriffit@student.42lyon.fr --role=administrator --user_pass=42School
-    wp user update 2 --allow-root --user_pass=42School
+    wp core install --url=$USER.42.fr --title="My Awsome Wordpress Website" --admin_user=$USER --admin_password=${PASS_USER} --skip-email
+    wp user create --allow-root ${USER} ${USER}@student.42lyon.fr --role=administrator --user_pass=${PASS_USER}
+    wp user update 2 --allow-root --user_pass=${PASS_USER}
 else
     echo "BYE"
 fi
-
-#php-fpmversion -F
+#/bin/bash
+/run/php/php-fpm8.1 -F
