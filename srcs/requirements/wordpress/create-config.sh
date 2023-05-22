aecc478e406f58984d20c2d6114f726dd36c85ad
sansho88 <sansho88@hotmail.fr>
2023-05-22 17:51:40 +0200

    Nginx is working. Can access to Wordpress Dashboard from tgriffit.42.fr or localhost:443

--- srcs/requirements/wordpress/create-config.sh
+++ srcs/requirements/wordpress/create-config.sh
@@ -3,7 +3,7 @@ if [ ! -f "/var/www/html/wp-config.php" ];
 then
     while ! mysql -h mariadb --user=$USER --password=$PASS_USER -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$DB_NAME'";do
             echo "Database is creating..."
-            sleep 8;
+            sleep 2;
     done
     echo "==> ${DB_NAME} is created ! "
 #    sudo mkdir -p ${WP_PATH}
@@ -12,7 +12,7 @@ then
     wp config create --allow-root --dbname=${DB_NAME} --dbuser=${USER} --dbpass=${PASS_USER} --dbhost=mariadb --extra-php --force --skip-check
 
     echo "==> Wordpress config file created"
-    wp core install --path=${WP_PATH} --allow-root  --skip-email --url=$USER.42.fr --title="My Awesome Wordpress Website" --admin_user=$USER --admin_password=${PASS_USER} --admin_email=${USER}@student.42lyon.fr
+    wp core install --path=${WP_PATH} --allow-root  --skip-email --url=${URL} --title="My Awesome Wordpress Website" --admin_user=$USER --admin_password=${PASS_USER} --admin_email=${USER}@student.42lyon.fr
     chmod -R +rwx ${WP_PATH}
     sudo -u tgriffit -i -- wp user create --path=${WP_PATH} king king@student.42lyon.fr --role=administrator --user_pass=${PASS_ROOT}
     echo "==> Updating users.. (~2 minutes)"
